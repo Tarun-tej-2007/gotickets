90d7f390688b6ab4ad6892b44a8e8402ea16d045
Tarun-tej-2007 <taruntej947@gmail.com>
2025-09-02 15:29:02 +0530

    completed the login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./login.css";
 
-function login() {
+export default function login() {
   return (
-    <div>login</div>
-  )
-}
+    <div className="page">
+      <h1>Sign in page</h1>
+      <div className="form-container">
+        <h2>Create an account</h2>
+        <form>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" />
 
-export default login
+          <label htmlFor="email">Email Id</label>
+          <input type="email" id="email" name="email" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+
+          <label htmlFor="repassword">Re-Enter Password</label>
+          <input type="password" id="repassword" name="repassword" />
+
+          <button type="submit">Sign in</button>
+        </form>
+      </div>
+    </div>
+  );
+}
