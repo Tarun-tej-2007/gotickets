99aa4219744ef954c40f542b4ebd9a1b526e504c
Tarun-tej-2007 <taruntej947@gmail.com>
2025-09-26 11:46:37 +0530

    initialised navbar

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,11 @@
 const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT , () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
+
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
-
-
-
-
-function navbar() {
+function navbar(){
   return (
-    <div>navbar</div>
+    <nav>
+      <div>
+        <h2>
+          <Link to='/welcome'>
+            <button>GoRooms</button>
+          </Link>
+        </h2>
+      </div>
+      <div>
+        <p>
+          <Link to='/login'>
+            <button>Login</button>
+          </Link>
+        </p>
+        <p>
+          <Link to='/signup'>
+            <button>Sign up</button>
+          </Link>
+        </p>
+      </div>
+    </nav>
   )
 }
+
+
+
+
+
 
 export default navbar
